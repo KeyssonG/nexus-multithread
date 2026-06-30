d83453b15f058797356f6ca057e69104e7af9313
keyssong <keyssoncardoso0@gmail.com>
2026-06-29 21:54:30 -0300

    chore: reduz verbosidade dos logs em producao

--- libs/common-core/src/main/java/keysson/nexus/common/config/CommonSwaggerConfig.java
+++ libs/common-core/src/main/java/keysson/nexus/common/config/CommonSwaggerConfig.java
@@ -4,19 +4,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 @Configuration
 public class CommonSwaggerConfig {
 
-    @Bean
-    public WebSecurityCustomizer swaggerSecurityCustomizer() {
-        return web -> web.ignoring()
-                .requestMatchers(
-                        "/swagger-ui/**",
-                        "/v3/api-docs/**",
-                        "/swagger-ui.html",
-                        "/webjars/**"
-                );
-    }
 }
