a7e253d391cabc252121284654ce5426412fc196
KeyssonG <keyssoncardoso0@gmail.com>
2026-07-22 22:33:23 -0300

    fix(estoque): adicionar import de @Size em CadastrarProdutoRequest

--- modules/estoque/src/main/java/keysson/apis/estoque/dto/request/CadastrarProdutoRequest.java
+++ modules/estoque/src/main/java/keysson/apis/estoque/dto/request/CadastrarProdutoRequest.java
@@ -3,6 +3,7 @@ package keysson.apis.estoque.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 
