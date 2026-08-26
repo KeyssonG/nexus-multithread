866be86c7441e0e187978919e1c09e55bab4325d
KeyssonG <keyssoncardoso0@gmail.com>
2026-08-25 23:58:33 -0300

    fix: DualAuthTokenFilter injeta JwtUtil do common-core (sem qualifier)

--- libs/common-core/src/main/java/keysson/nexus/security/DualAuthTokenFilter.java
+++ libs/common-core/src/main/java/keysson/nexus/security/DualAuthTokenFilter.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -24,7 +23,6 @@ public class DualAuthTokenFilter extends OncePerRequestFilter {
     private static final Logger log = LoggerFactory.getLogger(DualAuthTokenFilter.class);
 
     @Autowired
-    @Qualifier("validacaoADJwtUtil")
     private JwtUtil legacyJwtUtil;
 
     @Override
